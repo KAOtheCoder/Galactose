969b44e39577903ff98ca9e5c03efb667b962cb9
KAOtheCoder <kaolipro@gmail.com>
2023-07-15 17:44:01 +0300

    Fix trying to access cleared layer

--- GalactoseEditor/src/main.cpp
+++ GalactoseEditor/src/main.cpp
@@ -13,19 +13,19 @@ int main(int argc, char** argv) {
 
 	std::filesystem::path projectFilePath;
 
-	{
-		auto window = Window::create("Galactose Hub", 1366, 768);
+	{ // Open Hub
+		const auto& window = Window::create("Galactose Hub", 1366, 768);
 		window->layers.push_back(std::make_shared<HubLayer>(window.get()));
 
 		const int exitCode = app.exec();
 		if (exitCode != 0)
 			return exitCode;
 
-		// TODO: fix nullptr window
-		projectFilePath = static_cast<HubLayer*>(window->layers.front().get())->projectFilePath();
+		if (!window->isClosed()) // layers cleared on close
+			projectFilePath = static_cast<HubLayer*>(window->layers.front().get())->projectFilePath();
 	}
 
-	if (!projectFilePath.empty()) {
+	if (!projectFilePath.empty()) { // Open Editor
 		auto window = Window::create("Galactose", 1920, 1080);
 		auto renderer = Renderer::create(window.get());
 		window->layers.push_back(std::make_shared<EditorLayer>(window.get(), projectFilePath));
@@ -33,5 +33,5 @@ int main(int argc, char** argv) {
 		return app.exec();
 	}
 
-	return -1;
+	return 0;
 }
